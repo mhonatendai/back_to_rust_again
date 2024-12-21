f50fff775ebb6d6b38bc6f460bd96cbcbdcf5a7b
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-21 03:16:56 -0500

    Adding mutable x, using mut to change its immutability nature.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("This is just a simple program to check the setup!");
+
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
