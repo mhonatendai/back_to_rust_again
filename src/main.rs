5f06c1c6ede1ff40b8e674fff38a50f95c19098d
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-21 05:21:23 -0500

    Refactoring.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ const ONE_HOUR_IN_SECONDS: u32 = 60 * 60;
 fn main() {
     println!("This is just a simple program to check the setup!");
 
+
+    //You cannot reassign a variable in Rust without making it explicitly as mutable but you can redeclare it.
+    let y = 5;
+    println!("The value of y is: {y}");
+    let y = 6;
+    println!("The value of y is: {y}");
+
     let x = 5;
 
     let x = x + 1;
@@ -12,11 +19,11 @@ fn main() {
         println!("The value of x in the inner scope is: {x}");
     }
 
-    println!("The value of x is: {x}");
+    println!("The value of x is: {x}, outside the block");
+
     //Constants example
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
     println!("The value of 3 hours in seconds is: {THREE_HOURS_IN_SECONDS}");
     println!("The value of one hour in seconds is: {ONE_HOUR_IN_SECONDS}");
-
 }
